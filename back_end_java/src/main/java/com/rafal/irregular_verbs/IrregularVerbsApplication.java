1b8c4aeb95d8a8d31a75d94857cf4b81eeee0e7e
gonzicz <gonzicz83@gmail.com>
2019-09-11 17:58:34 +0200

    first commit

---
+++ back_end_java/src/main/java/com/rafal/irregular_verbs/IrregularVerbsApplication.java
@@ -0,0 +1,13 @@
+package com.rafal.irregular_verbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrregularVerbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrregularVerbsApplication.class, args);
+	}
+
+}
